709abb156d45c5fb47162570a19bc0d4007fb24e
Keith Howling <kehowli@microsoft.com>
2016-07-22 23:46:47 +0100

    ok

--- test-bench.js
+++ test-bench.js
@@ -15,12 +15,12 @@ if (cluster.isMaster) {
     
 
     const redis = new Redis('redis://localhost:6379/3');
-    redis.on("error", (c) => console.log (`error ${c}`));
+    redis.on("error", (c) => console.log (`redis error: ${JSON.stringify(c)}`));
     redis.on("ready", (c) => {
         console.log ('redis ready');
         const REDIS_CHANNEL = 'clickpath';
         let promiseArray = [];
-        for (var l = 0; l < (process.env.MESSAGES || 1000); l++) {
+        for (var l = 0; l < (process.env.MESSAGES || 10000); l++) {
             //let publish_promise = redis.publish(REDIS_CHANNEL,  `{"itteration": ${l}, "workerid": ${cluster.worker.id}, "message": "hello from ${cluster.worker.id}:${l}"}`);
             let publish_promise = redis.lpush(REDIS_CHANNEL,  `{"itteration": ${l}, "workerid": ${cluster.worker.id}, "message": "hello from ${cluster.worker.id}:${l}"}`);
             if (process.env.DELAY) {
@@ -29,7 +29,7 @@ if (cluster.isMaster) {
                 promiseArray.push (publish_promise);
             }
         }
-        promiseArray.reduce((p, fn) => p.then(fn, (err) => console.log (err)), Promise.resolve()).then((res) => {
+        promiseArray.reduce((p, fn) => p.then(fn, (err) => console.log (`send error : ${JSON.stringify(err)}`)), Promise.resolve()).then((res) => {
             console.log (`finished ${cluster.worker.id}`);
             //redis.quit();
             process.exit(0);
