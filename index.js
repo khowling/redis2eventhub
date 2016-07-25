bbb20918a665611f7b79694ea61b9d91332cefa1
Keith Howling <kehowli@microsoft.com>
2016-07-25 11:34:49 +0100

    remove cluster

--- index.js
+++ index.js
@@ -2,96 +2,89 @@
 const eventHub = require('azure-event-hubs').Client
 const Redis = require('ioredis')
 
-const cluster = require('cluster')
-const numCPUs = require('os').cpus().length
-if (cluster.isMaster) {
-    // Fork workers.
-    for (var i = 0; i < (process.env.WORKERS || numCPUs); i++) {
-        cluster.fork()
-    }
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`worker ${worker.process.pid} exit`)
-    });
-} else {
-    console.log(`worker started #${cluster.worker.id}`)
-
-    const redis = new Redis('redis://localhost:6379/3', {
-    enableOfflineQueue: true,
-    reconnectOnError: function (err) {
-        console.log(`redis err ${err}`)
-    },
-    retryStrategy: function (times) {
-        var delay = Math.min(times * 2, 2000)
-        return delay
-    }
-    });
-    redis.on("ready", (c) => console.log ('redis ready'))
-    redis.on("error", (c) => console.log (`redis error: ${JSON.stringify(c)}`))
-
-
-    const connectionString = 'Endpoint=sb://mapusageingest-ns.servicebus.windows.net/;SharedAccessKeyName=RedisSend;SharedAccessKey=PHyXTDY09cfC3pIpCLKc5cHX8rB6tGPrROa4u+vG6dc=;EntityPath=mapusageingest'
-    const eventHubPath = '/clickpath'
-    const partitionId = '0'
-
-    // -------------------- Connect to Azure Event Hub
-    var client = eventHub.fromConnectionString(connectionString, eventHubPath);
-    client.createSender(partitionId).then((sender) => {
-        sender.on('errorReceived',  (err) => { console.log(err); })
-
-        console.log (`#${cluster.worker.id} Created EventHub Sender`)
-
-        const REDIS_CHANNEL = 'clickpath'
-
-        /* -- uncomment if using pub/sub
-        redis.subscribe(REDIS_CHANNEL,  (err, count) => {
-            if (err) {
-                process.exitCode = 1;
-                throw new Error(`cannot subscribe to redis channel ${REDIS_CHANNEL}`);
-            } 
-            console.log (`subscribed to channel ${REDIS_CHANNEL} (#=${count})`);
-        });
-        redis.on('message', (channel, message) => {
-        */
-
-        // pop loop
-        const max_send_waiting = process.env.CONCURRENT || 20
-        let send_waiting = 0 , totalsent = 0, totalerr = 0, e = totalerr, w = send_waiting, t = totalsent
-            subscribe = () => {
-                redis.brpop(REDIS_CHANNEL, 0).then ((message) => {
-                    sender.send(message).then ((res) => {
-                        // promises are resolved when a disposition frame is received from the remote link for the sent message, at this point the message is considered "settled". 
-                        send_waiting--; totalsent++
-                        if (send_waiting == (max_send_waiting - 1)) subscribe();
-                        //console.log (`sent: ${JSON.stringify(res)}`);
-                    }, (err) => {
-                        send_waiting--; totalerr++
-                        if (send_waiting == (max_send_waiting - 1)) subscribe();
-                        console.log (`send error: ${JSON.stringify(err)}`)
-                    });
-
-                    send_waiting++;
-                    if (send_waiting < max_send_waiting) subscribe();
-                });
-            };
-
-        // print metrics if they change
-        setInterval(() => {
-            if (send_waiting !== w || totalsent !== t) {
-                w = send_waiting; t = totalsent
-                console.log (`#${cluster.worker.id} totalerr: ${totalerr}, totalsent: ${totalsent}, inprogress: ${send_waiting}`)
-            }
-        }, 1000);
-
-        subscribe();
-
-        // close down connections on ctrl-c
-        process.on ('SIGINT', (code) => {
-            try {
-                sender.close().then((res) => console.log ('closed sender'));
-                console.log ('closed connetions');
-            } catch (e) {
-
-            }
-        })
-    });
+const redis = new Redis('redis://localhost:6379/3', {
+enableOfflineQueue: true,
+reconnectOnError: function (err) {
+    console.log(`redis err ${err}`)
+},
+retryStrategy: function (times) {
+    var delay = Math.min(times * 2, 2000)
+    return delay
 }
+});
+redis.on("ready", (c) => console.log ('redis ready'))
+redis.on("error", (c) => console.log (`redis error: ${JSON.stringify(c)}`))
+
+
+const connectionString = 'Endpoint=sb://mapusageingest-ns.servicebus.windows.net/;SharedAccessKeyName=RedisSend;SharedAccessKey=PHyXTDY09cfC3pIpCLKc5cHX8rB6tGPrROa4u+vG6dc=;EntityPath=mapusageingest'
+const eventHubPath = '/clickpath'
+const partitionId = '0'
+
+// -------------------- Connect to Azure Event Hub
+var client = eventHub.fromConnectionString(connectionString, eventHubPath);
+client.createSender(partitionId).then((sender) => {
+    sender.on('errorReceived',  (err) => { console.log(err); })
+
+    console.log (`Created EventHub Sender`)
+
+    const REDIS_CHANNEL = 'clickpath'
+
+    /* -- uncomment if using pub/sub
+    redis.subscribe(REDIS_CHANNEL,  (err, count) => {
+        if (err) {
+            process.exitCode = 1;
+            throw new Error(`cannot subscribe to redis channel ${REDIS_CHANNEL}`);
+        } 
+        console.log (`subscribed to channel ${REDIS_CHANNEL} (#=${count})`);
+    });
+    redis.on('message', (channel, message) => {
+    */
+
+    // pop loop
+    const max_send_waiting = process.env.CONCURRENT || 20
+    let sent = 0 , confirmed = 0, errors = 0,
+        subscribe = () => {
+            redis.brpop(REDIS_CHANNEL, 0).then ((message) => {
+                sender.send(message).then ((res) => {
+                    // promises are resolved when a disposition frame is received from the remote link for the sent message, at this point the message is considered "settled". 
+                    confirmed++; 
+            //      if (send_waiting == (max_send_waiting - 1)) 
+            //         subscribe();
+                    //console.log (`sent: ${JSON.stringify(res)}`);
+                }, (err) => {
+                    errors++;
+            //      if (send_waiting == (max_send_waiting - 1)) 
+            //         subscribe();
+                    console.log (`send error: ${JSON.stringify(err)}`)
+                });
+
+                sent++
+                //  if (send_waiting < max_send_waiting) 
+                    subscribe()
+            });
+        };
+
+    // print metrics if they change
+    var l = -1, s = 0, c = 0, e = 0
+    setInterval(() => {
+        //redis.llen(REDIS_CHANNEL).then( (llen) => {
+            if (sent !== s || confirmed !== c || errors !== e) {
+                s = sent; c = confirmed; e = errors
+                console.log (`errors: ${errors}, confirmed: ${confirmed}, inprogress: ${sent - confirmed}`)
+            }
+        //}, (err) => console.log (`cannot get list length ${err}`))
+    }, 2000);
+
+    subscribe();
+
+    // close down connections on ctrl-c
+    process.on ('SIGINT', (code) => {
+        try {
+            sender.close().then((res) => console.log ('closed sender'));
+            console.log ('closed connetions');
+            process.exit(0);
+        } catch (e) {
+
+        }
+    })
+});
--- test-bench.js
+++ test-bench.js
@@ -2,11 +2,14 @@ const Redis = require('ioredis');
 
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
+
+const testLongStr = 'Around 250,000 people making their way to the Port of Dover were stuck in traffic for 15 hours yesterday (centre) - with many motorists forced to sleep in their vehicles overnight. The reason for the chaos was heightened French security on this side of the channel in the wake of the terror attack in Nice last week. Port authorities said French border control booths at Dover had been "seriously understaffed" overnight, with just three of the seven passport control booths open. At the height of the mayhem, just one member of the French border force was checking passengers" passports on hundreds of coaches - taking 40 minutes to check each coach. Rob Jackson (top right) left Golcar in West Yorkshire with his partner and two children at 8am Saturday morning and were still stuck in traffic some 15 hours later. Social media users on Twitter have questioned whether the turmoil in Kent is a "2Brexit hate crime" while others have suggested the French authorities are giving Brits a "hard time" by "making it awkward" following the referendum. One user suggested the hold another referendum vote on the M20 (inset) near Dover - just 20 miles from Calais in France. '
+
 if (cluster.isMaster) {
     // Fork workers.
-    for (var i = 0; i < (process.env.WORKERS || numCPUs); i++) {
+    //for (var i = 0; i < (process.env.WORKERS || numCPUs); i++) {
         cluster.fork();
-    }
+    //}
     cluster.on('exit', (worker, code, signal) => {
         console.log(`worker ${worker.process.pid} exit`);
     });
@@ -20,9 +23,9 @@ if (cluster.isMaster) {
         console.log ('redis ready');
         const REDIS_CHANNEL = 'clickpath';
         let promiseArray = [];
-        for (var l = 0; l < (process.env.MESSAGES || 10000); l++) {
+        for (var l = 0; l < (process.env.MESSAGES || 1); l++) {
             //let publish_promise = redis.publish(REDIS_CHANNEL,  `{"itteration": ${l}, "workerid": ${cluster.worker.id}, "message": "hello from ${cluster.worker.id}:${l}"}`);
-            let publish_promise = redis.lpush(REDIS_CHANNEL,  `{"itteration": ${l}, "workerid": ${cluster.worker.id}, "message": "hello from ${cluster.worker.id}:${l}"}`);
+            let publish_promise = redis.lpush(REDIS_CHANNEL,  `{"body": ${testLongStr}, itteration": ${l}, "workerid": ${cluster.worker.id}, "message": "hello from ${cluster.worker.id}:${l}"}`);
             if (process.env.DELAY) {
                 promiseArray.push (() => {return new Promise((a,b) => setTimeout (() => {console.log (`send from ${cluster.worker.id}/${l}`); publish_promise.then(() => a());}, process.env.DELAY))});
             } else {
